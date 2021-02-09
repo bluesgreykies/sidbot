5e42db9ade7abb7a56f80fe846f49d2f583176a4
bluesgreykies <78812902+bluesgreykies@users.noreply.github.com>
2021-02-09 17:16:43 +0000

    Remove Enmap to fix issue #1 on main Sidbot

--- sidbot.js
+++ sidbot.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const Enmap = require("enmap");
 const fs = require("fs");
 const humanizeDuration = require('humanize-duration');
 const client = new Discord.Client();
@@ -20,7 +19,7 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
-client.commands = new Enmap();
+client.commands = new Map();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
